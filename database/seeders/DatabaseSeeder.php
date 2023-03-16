9c76d19887029746eee75d81c65b0d54d94b2259
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-16 10:10:09 +0100

    add relation between User and Post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         User::factory(10)->create();
 
         
-        \App\Models\User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
