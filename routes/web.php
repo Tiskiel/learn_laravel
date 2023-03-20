f09cc6a71a3c2693aec7f6f4c71487192edc8bf8
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-20 14:24:34 +0100

    add auto overflow and max height to dropdown

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::get('/', function () {
         "posts" => Post::latest()->get(),
         "categories" => Category::all()
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post}', function (Post $post) {
     return view('post', [
@@ -37,7 +37,7 @@ Route::get('categories/{category}', function(Category $category) {
         'currentCategory' => $category,
         "categories" => Category::all()
     ]);
-});
+})->name('category');
 
 Route::get('author/{author:username}', function(User $author) {
     return view('posts', [
