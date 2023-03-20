b7a6baef334b03a78417f20d92fa7ccb4e835ae7
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-20 10:21:18 +0100

    render post page dynamic

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,31 +1,13 @@
 <x-layout>
-        @include('_posts-header')
+    @include('_posts-header')
 
-        <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-            @if ( $posts->count() )
-                <x-posts-grid :posts="$posts" />
-            @else
-                <p class="text-center">No posts. Please check back later</p>
-            @endif
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ( $posts->count() )
+            <x-posts-grid :posts="$posts" />
+        @else
+            <p class="text-center">No posts. Please check back later</p>
+        @endif
 
-        </main>
-
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h1>
-                <a href="/posts/{{$post->slug}}">
-                    {{$post->title}}
-                </a>
-            </h1>
-
-            <p>
-                By <a href="/author/{{ $post->author->username }}">{{ $post->author->username }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-
-            <p>
-                {{$post->excerpt}}
-            </p>
-        </article>
-    @endforeach --}}
+    </main>
 </x-layout>
 
