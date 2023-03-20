6690d2b1a3a2d0f426c723052ec3373f5b6a1a2a
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-20 13:54:06 +0100

    fix bad concatenation

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -35,12 +35,12 @@
                     </button>
                 </x-slot>
     
-                <x-dropdown-item href="/">All</x-dropdown-item>
+                <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
 
                 @foreach ( $categories as $category )
                     <x-dropdown-item
                     href="/categories/{{ $category->slug }}"
-                    :active="isset($currentCategory) && $currentCategory->is($category)"
+                    :active="request()->is('categories/' .$category->slug)"
                     >{{ ucwords($category->name) }}</x-dropdown-item>
                 @endforeach
             </x-dropdown>
