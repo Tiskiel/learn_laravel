2f333e22416c1b67795359731dccf137e91def06
Jean-François <jf@Jean-Francoiss-MacBook-Air.local>
2023-03-22 22:12:30 +0100

    add interface for hard type class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
+use App\Services\MailchumpNewsletter;
+use App\Services\Newsletter;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use MailchimpMarketing\ApiClient;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +15,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        app()->bind(Newsletter::class, function () {
+            $client = (new ApiClient)->setConfig([
+                'apiKey' => config('services.mailchimp.key'),
+                'erver' => 'us11'
+            ]);
+
+            return new MailchumpNewsletter($client);
+        });
     }
 
     /**
